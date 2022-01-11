3815b14c59e17c450e7f5ce37ed700c23b6cd239
TestUndervisning <hansen.pauline@yahoo.dk>
2022-01-11 14:45:44 +0100

    Initial commit

---
+++ src/main/java/data/PatientDAO.java
@@ -0,0 +1,8 @@
+package data;
+
+public class PatientDAO {
+
+    public void addPatient(PatientData patient){
+
+    }
+}
---
+++ src/main/java/data/PatientData.java
@@ -0,0 +1,22 @@
+package data;
+
+public class PatientData {
+    private String name;
+    private String cpr;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+
+}
