4cd5c7cc9f3a5c43ad1146ce88801728c5314bf2
BILGIN <busra2000@live.dk>
2022-01-13 13:31:30 +0100

    13/1

--- src/main/webapp/js/opretPatient.js
+++ src/main/webapp/js/opretPatient.js
@@ -2,13 +2,13 @@ async function opretPatient() {
     let formElement = document.getElementById("formOP");
     let formData = new FormData(formElement);
     let json = Object.fromEntries(formData);
-    let res = await fetch(
-        "rest/patienter", {
+    let res = await fetch("rest/patienter",{
             method: "POST",
             body: JSON.stringify(json),
             headers: {
                 "content-type": "application/json",
                 "authorization": "Bearer " + localStorage.getItem("token")
             }
+
         })
 }
